2ec61417a7cdd121e72eea8694a9bb4960ca4599
tsikoviski <tsikoviski1@gmail.com>
2024-05-05 06:45:23 -0700

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sqlite3
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('radios.db')
     conn.row_factory = sqlite3.Row
     return conn
 
